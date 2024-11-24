a49947ef6f8a99fb0b5469b1c5afbce357048642
bhoomi874u <bs4374585@gmail.com>
2024-11-24 10:03:59 +0530

    program of area and circumfrance

---
+++ area and circumfrence.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+	int r;
+	float pi=3.14 , a,c;
+	printf("please  enter a value");
+	scanf("%d",&r);
+	a=3.14*r*r;
+	printf("The area of circle=%d",a);
+//	printf("The circumfrance of circle=%d",);
+//	scanf("%d",&c);
+	c=2*3.14*r;
+//	printf("%d",c);
+		printf("\nThe circumfrance of circle=%d",c);
+}
